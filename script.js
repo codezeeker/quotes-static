b21555f4e43de371463fa30138552d0d86026526
Code Zeek <73175466+codezeeker@users.noreply.github.com>
2024-05-02 15:02:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
-let currentCard = 1;
+// Define your quotes
+var quotes = "My quote 1|||My quote 2|||My quote 3|||My quote 4|||My quote 5";
 
+// Split the quotes into an array
+var quotesArray = quotes.split("|||");
+
+// Get the container
+var container = document.getElementById("cardContainer");
+
+// Populate quotes into cards
+quotesArray.forEach(function(quote, index) {
+    var card = document.createElement("div");
+    card.classList.add("cards");
+    card.innerText = quote;
+    card.style.display = index === 0 ? "block" : "none"; // Show first card, hide others
+    container.appendChild(card);
+});
+
+// Keep track of current card index
+var currentCardIndex = 0;
+
+// Get all cards
+var cards = document.getElementsByClassName("cards");
+
+// Function to rotate cards
 function rotateCard() {
-    const current = document.getElementById(`card${currentCard}`);
-    current.classList.remove('front');
-
-    currentCard++;
-    if (currentCard > 3) {
-        currentCard = 1;
-    }
-
-    const next = document.getElementById(`card${currentCard}`);
-    next.classList.add('front');
+    cards[currentCardIndex].style.display = "none"; // Hide current card
+    currentCardIndex = (currentCardIndex + 1) % cards.length; // Get next card index
+    cards[currentCardIndex].style.display = "block"; // Show next card
 }
