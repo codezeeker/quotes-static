41338734ea70a970496dd6d8d351ba3eb1546313
Code Zeek <73175466+codezeeker@users.noreply.github.com>
2024-05-02 14:27:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-function rotateCard(card) {
-    card.classList.toggle('flipped');
+let currentCard = 1;
+
+function rotateCard() {
+    const current = document.getElementById(`card${currentCard}`);
+    current.classList.remove('front');
+
+    currentCard++;
+    if (currentCard > 3) {
+        currentCard = 1;
+    }
+
+    const next = document.getElementById(`card${currentCard}`);
+    next.classList.add('front');
 }
